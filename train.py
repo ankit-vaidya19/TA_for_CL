201bcb989456a836e16848f23715f37c0ce0da22
Ankit Vaidya <62834930+ankit-vaidya19@users.noreply.github.com>
2023-09-18 16:52:32 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,49 @@
+from vit_baseline import ViT_LoRA, cfg
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from transformers import AutoImageProcessor
+
+processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+
+
+train_ds = datasets.OxfordIIITPet(
+    root="/mnt/d/TA_LORA",
+    split="trainval",
+    target_types="category",
+    download=False,
+    transform=transforms.Compose(
+        [
+            transforms.RandomResizedCrop(processor.size["height"]),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+        ]
+    ),
+)
+
+test_ds = datasets.OxfordIIITPet(
+    root="/mnt/d/TA_LORA",
+    split="test",
+    target_types="category",
+    download=False,
+    transform=transforms.Compose(
+        [
+            transforms.Resize(processor.size["height"]),
+            transforms.CenterCrop(processor.size["height"]),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+        ]
+    ),
+)
+
+train_loader = DataLoader(
+    train_ds, batch_size=cfg.batch_size, shuffle=True, num_workers=2
+)
+test_loader = DataLoader(
+    test_ds, batch_size=cfg.batch_size, shuffle=False, num_workers=2
+)
+
+model = ViT_LoRA(use_LoRA=True)
+model.fit(train_loader=train_loader)
+# model.test(test_loader=test_loader)
---
+++ vit_baseline.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+from transformers import ViTForImageClassification
+from peft import LoraConfig, get_peft_model
+from tqdm import tqdm
+import numpy as np
+from ml_collections import ConfigDict
+
+np.random.seed(42)
+torch.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(42)
+
+cfg = ConfigDict
+cfg.epochs = 50
+cfg.batch_size = 16
+cfg.lr = 5e-6
+cfg.weight_decay = 1e-6
+
+
+class ViT_LoRA(nn.Module):
+    def __init__(self, model_name="google/vit-base-patch16-224", use_LoRA=False):
+        super().__init__()
+        self.model_name = model_name
+        self.use_LoRA = use_LoRA
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(self.device)
+        if self.use_LoRA:
+            self.ViT = ViTForImageClassification.from_pretrained(self.model_name)
+            self.linear = nn.Linear(1000, 37)
+            self.config = LoraConfig(
+                r=16,
+                lora_alpha=16,
+                target_modules=["query", "value"],
+                lora_dropout=0.1,
+                bias="none",
+                modules_to_save=["classifier"],
+            )
+            self.ViT = get_peft_model(self.ViT, self.config)
+            self.print_trainable_parameters()
+        else:
+            self.ViT = ViTForImageClassification.from_pretrained(self.model_name)
+            self.linear = nn.Linear(1000, 37)
+            self.print_trainable_parameters()
+        self.ViT.to(self.device)
+        self.linear.to(self.device)
+
+    def forward(self, x):
+        return self.linear(self.ViT(x).logits)
+
+    def print_trainable_parameters(self):
+        trainable_params = 0
+        all_param = 0
+        for _, param in self.ViT.named_parameters():
+            all_param += param.numel()
+            if param.requires_grad:
+                trainable_params += param.numel()
+        print(
+            f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
+        )
+
+    def accuracy(self, true, pred):
+        true = np.array(true)
+        pred = np.array(pred)
+        acc = np.sum((true == pred).astype(np.float32)) / len(true)
+        return acc * 100
+
+    def fit(self, train_loader):
+        optim = torch.optim.Adam(
+            params=self.parameters(), lr=cfg.lr, weight_decay=cfg.weight_decay
+        )
+        criterion = nn.CrossEntropyLoss()
+        self.train()
+        for epoch in range(cfg.epochs):
+            print(f"{epoch}/{cfg.epochs}")
+            train_loss = []
+            train_preds = []
+            train_labels = []
+            for batch in tqdm(train_loader):
+                imgs = batch[0].to(self.device)
+                labels = batch[1].to(self.device)
+                scores = self(imgs)
+                loss = criterion(scores, labels)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+                train_loss.append(loss.detach().cpu().numpy())
+                train_labels.append(batch[1])
+                train_preds.append(scores.argmax(dim=-1))
+            print(f"Train Loss - {sum(train_loss)/len(train_loss)}")
+            print(
+                f"Train Accuracy - {self.accuracy(torch.concat(train_labels, dim=0).cpu(),torch.concat(train_preds, dim=0).cpu())}"
+            )
+
+    def test(self, test_loader):
+        criterion = nn.CrossEntropyLoss()
+        self.eval()
+        with torch.no_grad():
+            for epoch in range(cfg.epochs):
+                print(f"{epoch}/{cfg.epochs}")
+                train_loss = []
+                train_preds = []
+                train_labels = []
+                for batch in tqdm(test_loader):
+                    imgs = batch[0].to(self.device)
+                    labels = batch[1].to(self.device)
+                    scores = self(imgs)
+                    loss = criterion(scores, labels)
+                    train_loss.append(loss.detach().cpu().numpy())
+                    train_labels.append(batch[1])
+                    train_preds.append(scores.argmax(dim=-1))
+                print(f"Test Loss - {sum(train_loss)/len(train_loss)}")
+                print(
+                    f"Test Accuracy - {self.accuracy(torch.concat(train_labels, dim=0).cpu(),torch.concat(train_preds, dim=0).cpu(),)}"
+                )
