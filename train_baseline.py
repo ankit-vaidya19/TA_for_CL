2041c07b962bb1b0e113a2208f545266237318a8
Ankit Vaidya <62834930+ankit-vaidya19@users.noreply.github.com>
2023-09-24 23:28:25 +0530

    Add files via upload

--- train_baseline.py
+++ train_baseline.py
@@ -7,7 +7,7 @@ from transformers import AutoImageProcessor
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-d', '--data', type=str, default='oxfordpet')
+parser.add_argument('-d', '--data', type=str, default='oxfordpets')
 parser.add_argument('-ddir', '--data-dir', type=str, default='../data')
 parser.add_argument('-bs','--batch_size',type = int,default = 32)
 parser.add_argument('-nw','--num_workers',type=int,default=2)
@@ -21,33 +21,92 @@ args = parser.parse_args()
 
 processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
 
-train_ds = datasets.args.data(
-    root=args.ddrir,
-    split="train",
-    download=True,
-    transform=transforms.Compose(
-        [
-            transforms.RandomResizedCrop(processor.size["height"]),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
-        ]
-    ),
-)
 
-test_ds = datasets.OxfordIIITPet(
-    root=args.ddir,
-    split="test",
-    download=True,
-    transform=transforms.Compose(
-        [
-            transforms.Resize(processor.size["height"]),
-            transforms.CenterCrop(processor.size["height"]),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
-        ]
-    ),
-)
+
+if args.data == "oxfordpets":
+    train_ds = datasets.OxfordIIITPet(
+        root=args.ddrir,
+        split="trainval",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.RandomResizedCrop(processor.size["height"]),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+
+    test_ds = datasets.OxfordIIITPet(
+        root=args.ddir,
+        split="test",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(processor.size["height"]),
+                transforms.CenterCrop(processor.size["height"]),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+elif args.data == "svhn":
+    train_ds = datasets.SVHN(
+        root=args.ddrir,
+        split="train",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.RandomResizedCrop(processor.size["height"]),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+
+    test_ds = datasets.SVHN(
+        root=args.ddir,
+        split="test",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(processor.size["height"]),
+                transforms.CenterCrop(processor.size["height"]),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+elif args.data == "oxfordflowers":
+    train_ds = datasets.Flowers102(
+        root=args.ddrir,
+        split="train",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.RandomResizedCrop(processor.size["height"]),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+
+    test_ds = datasets.Flowers102(
+        root=args.ddir,
+        split="test",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(processor.size["height"]),
+                transforms.CenterCrop(processor.size["height"]),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
 
 train_loader = DataLoader(
     train_ds, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers
