6e6a6049590afa6763ccad0c4c3ad24c17bd5d8d
Ankit Vaidya <62834930+ankit-vaidya19@users.noreply.github.com>
2023-09-25 18:12:59 +0530

    Made changes to dataloader and added argparse

--- test_model.py
+++ test_model.py
@@ -19,6 +19,14 @@ parser.add_argument('-nw','--num_workers',type=int,default=2)
 parser.add_argument('-midir', '--model-input-dir', type=str, default='./data')
 parser.add_argument('-tot', '--total-tasks', type=int)
 parser.add_argument('-sc','--scaling_coefficient',type = float,default=0.25)
+parser.add_argument('-e', '--epochs', type=int, default=50)
+parser.add_argument('-bs', '--batch-size', type=int, default=16)
+parser.add_argument('-lr', '--lr', type=float, default=5e-6)
+parser.add_argument('-wd', '--weight-decay', type=float, default=1e-6)
+parser.add_argument('--lora-r', type=int, default=16, help="The dimension used by the LoRA update matrices")
+parser.add_argument('--lora-alpha', type=int, default=16, help="scaling factor")
+parser.add_argument('--lora-dropout', type=float, default=0.1)
+parser.add_argument('--lora-bias', type=str, default="none", help="if bias params should be trained or not")
 args = parser.parse_args()
 
 def accuracy(true,pred):
@@ -51,21 +59,50 @@ def test_model(model,test_loader):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
 
-test_dataset = datasets.args.data(
-    root=args.ddir,
-    split="test",
-    download=True,
-    transform=transforms.Compose(
-        [
-            transforms.Resize(processor.size["height"]),
-            transforms.CenterCrop(processor.size["height"]),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
-        ]
-    ),
-)
+if args.data == "oxfordpets":
+    test_ds = datasets.OxfordIIITPet(
+        root=args.ddir,
+        split="test",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(processor.size["height"]),
+                transforms.CenterCrop(processor.size["height"]),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+elif args.data == "svhn":
+    test_ds = datasets.SVHN(
+        root=args.ddir,
+        split="test",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(processor.size["height"]),
+                transforms.CenterCrop(processor.size["height"]),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
+elif args.data == "oxfordflowers":
+    test_ds = datasets.Flowers102(
+        root=args.ddir,
+        split="test",
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(processor.size["height"]),
+                transforms.CenterCrop(processor.size["height"]),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
+            ]
+        ),
+    )
 
-test_loader = DataLoader(test_dataset,args.batch_size,num_workers=args.num_workers,shuffle=False)
+test_loader = DataLoader(test_ds,args.batch_size,num_workers=args.num_workers,shuffle=False)
 final_model = get_model(args, f"{args.output_dir}/vit_pretrained.pt", list_of_task_checkpoints=[f"{args.model_input_dir}/vit_task_{i}_best.pt" for i in range(args.total_tasks)], scaling_coef=args.scaling_coefficient)
 
 test_model(final_model,test_loader)
