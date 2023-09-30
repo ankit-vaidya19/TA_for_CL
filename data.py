0d2aead88a7898836e20608de584af4141ba9bba
rajas1310 <rajaschitale@gmail.com>
2023-09-30 07:30:32 +0000

    n sample loader.. to be continued...

--- data.py
+++ data.py
@@ -209,3 +209,138 @@ class TaskDataset():
         return ImageDataset(self.train_imgs, self.train_labels, 'train', self.img_processor), ImageDataset(self.test_imgs, self.test_labels, 'test', self.img_processor)
 
 
+class FewShotDataset():
+    def __init__(self, dataset_name, data_directory, img_processor, samples_per_class=10, task_dict=task_dict):
+        self.n_samples = samples_per_class
+        self.data = dataset_name
+        self.data_dir = data_directory
+        self.img_processor = img_processor
+        self.task_dict = task_dict[self.data]
+        self.label2int = self.get_label2int()
+        self.num_classes = len(self.label2int)
+        self.train_imgs, self.train_labels = [],[]
+        self.test_imgs, self.test_labels = [],[]
+        self.get_lists()
+        get_n_samples_per_class()
+        print(f"\nINFO: Taken {self.args.n_samples} samples for every class.")
+
+    def get_label2int(self):
+        label2int = []
+        for task in self.task_dict.values():
+            for clas in task:
+                label2int.append(clas)
+
+        keys = [x for x in range(len(label2int))]
+        label2int = dict(zip(label2int, keys))
+        return label2int
+
+    def get_lists(self):
+        if self.args.data == 'oxfordpet':
+            oxfordpet = datasets.OxfordIIITPet(root=self.args.data_dir, download=True)
+
+            #trainset lists
+            with open(f'{self.args.data_dir}/oxford-iiit-pet/annotations/trainval.txt', 'r') as f_train:
+                lines = f_train.readlines()
+                # print(lines)
+                for img in lines:
+                    img_name = img.split(' ')[0]
+                    img_label = img_name.rsplit('_', 1)[0]
+                    self.train_imgs.append(f'{self.args.data_dir}/oxford-iiit-pet/images/{img_name}.jpg')
+                    self.rain_labels.append(self.label2int[img_label])
+
+            #testset lists
+            with open(f'{self.args.data_dir}/oxford-iiit-pet/annotations/test.txt', 'r') as f_test:
+                lines = f_test.readlines()
+                for img in lines:
+                    img_name = img.split(' ')[0]
+                    img_label = img_name.rsplit('_', 1)[0]
+                    self.test_imgs.append(f'{self.args.data_dir}/oxford-iiit-pet/images/{img_name}.jpg')
+                    self.test_labels.append(self.label2int[img_label])   
+        
+        elif self.args.data == 'svhn':
+            trainset = datasets.SVHN(root=self.args.data_dir,
+                            split='train', download=True,
+                            transform=transforms.ToTensor())
+            testset = datasets.SVHN(root=self.args.data_dir,
+                            split='test', download=True,
+                            transform=transforms.ToTensor())
+            
+            train_data = sio.loadmat(f'{self.args.data_dir}/train_32x32.mat')
+            x_train = train_data['X']
+            y_train = train_data['y']
+            
+            test_data = sio.loadmat(f'{self.args.data_dir}/test_32x32.mat')
+            x_test = test_data['X']
+            y_test = test_data['y']
+
+            #for trainlist
+            dest_dir = f'{self.args.data_dir}/train'
+            os.makedirs(dest_dir, exist_ok=True)
+            
+            for idx in range(len(y_train)):
+                img = x_train[:,:,:,idx]
+                img_path = f'{dest_dir}/{idx}.jpg'
+                img_label = y_train.flat[idx]
+                cv2.imwrite(img_path, img)
+                self.train_imgs.append(img_path)
+                self.train_labels.append(img_label)
+            
+            #for testlist
+            dest_dir = f'{self.args.data_dir}/test'
+            os.makedirs(dest_dir, exist_ok=True)
+            
+            for idx in range(len(y_test)):
+                img = x_test[:,:,:,idx]
+                img_path = f'{dest_dir}/{idx}.jpg'
+                img_label = y_test.flat[idx]
+                cv2.imwrite(img_path, img)
+                self.test_imgs.append(img_path)
+                self.test_labels.append(img_label)
+       
+        elif self.args.data == 'oxfordflowers':
+            trainset = datasets.Flowers102(root=self.args.data_dir,
+                            split='train', download=True,
+                            transform=transforms.ToTensor())
+            testset = datasets.Flowers102(root=self.args.data_dir,
+                            split='test', download=True,
+                            transform=transforms.ToTensor())
+
+            labels = sio.loadmat(f'{self.args.data_dir}/flowers-102/imagelabels.mat')
+            labels = labels['labels'][0]
+            data_splits = sio.loadmat(f'{self.args.data_dir}/flowers-102/setid.mat')
+            x_train = data_splits['trnid'][0]
+            x_val = data_splits['valid'][0]
+            x_test = data_splits['tstid'][0]
+            
+            # trainset lists
+            for img_id in x_train:
+                img_path = f'{self.args.data_dir}/flowers-102/jpg/image_{str(img_id).zfill(5)}.jpg'
+                img_label = labels[img_id-1]
+                self.train_imgs.append(img_path)
+                self.train_labels.append(self.label2int[img_label])
+
+            for img_id in x_val:
+                img_path = f'{self.args.data_dir}/flowers-102/jpg/image_{str(img_id).zfill(5)}.jpg'
+                img_label = labels[img_id-1]
+                self.train_imgs.append(img_path)
+                self.train_labels.append(self.label2int[img_label])
+            
+            # testset lists
+            for img_id in x_test:
+                img_path = f'{self.args.data_dir}/flowers-102/jpg/image_{str(img_id).zfill(5)}.jpg'
+                img_label = labels[img_id-1]
+                self.test_imgs.append(img_path)
+                self.test_labels.append(self.label2int[img_label])
+
+        else:
+            # Download link for StanfordCars dataset is down 
+            # https://github.com/pytorch/vision/issues/7545#issuecomment-1575410733
+            pass
+    
+    def get_n_samples_per_class(self):
+        pass
+
+    def get_datasets(self):
+        print(f"INFO : Loading {self.args.data} TRAIN & TEST data for TASK {self.args.tasknum} ... ")
+        print("CLASSES : ", self.task_dict[self.args.tasknum])
+        return ImageDataset(self.train_imgs, self.train_labels, 'train', self.img_processor), ImageDataset(self.test_imgs, self.test_labels, 'test', self.img_processor)
