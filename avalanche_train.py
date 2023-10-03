1ccdb5e1c0699671dbb59bf15c8098ffb583588c
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2023-10-04 01:43:17 +0530

    Update avalanche_train.py

--- avalanche_train.py
+++ avalanche_train.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 # from peft import LoraConfig, get_peft_model
-from tqdm import tqdm
+# from tqdm import tqdm
 import numpy as np
 from avalanche.training.supervised import Naive, CWRStar, Replay, GDumb, Cumulative, LwF, GEM, AGEM, EWC  # and many more!
-from avalanche.logging import TextLogger
+from avalanche.logging import TextLogger, InteractiveLogger
 from avalanche.training.plugins import EvaluationPlugin
 from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics,\
     loss_metrics, timing_metrics, cpu_usage_metrics, StreamConfusionMatrix,\
@@ -12,16 +12,7 @@ from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics,\
 
 from avalanche_data import CustomDataset, ViT_basic
 
-
-def accuracy(true, pred):
-    true = np.array(true)
-    pred = np.array(pred)
-    acc = np.sum((true == pred).astype(np.float32)) / len(true)
-    return acc * 100
-
-def train():
-    pass
-
+from transformers import AutoImageProcessor
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -30,9 +21,9 @@ parser.add_argument('-e', '--epochs', type=int, default=50)
 parser.add_argument('-bs', '--batch-size', type=int, default=16)
 parser.add_argument('-lr', '--lr', type=float, default=5e-6)
 parser.add_argument('-wd', '--weight-decay', type=float, default=1e-6)
-parser.add_argument('-p', '--patience', type=int, default=10)
+# parser.add_argument('-p', '--patience', type=int, default=10)
 parser.add_argument('-nw','--num-workers', type=int, default=2)
-parser.add_argument('--test-interval', type=int, default=1)
+# parser.add_argument('--test-interval', type=int, default=1)
 parser.add_argument('--device', type=str, default="cuda:0")
 # parser.add_argument('--seed', type=int, default=42)
 
@@ -56,24 +47,26 @@ if args.num_classes == None:
     elif args.data == 'stanfordcars':
         args.num_classes = 196
 
-print(args)
+print(args, '\n')
 
+img_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
 
 
 model = ViT_basic(args)
-scenario = CustomDataset(args).get_scenario()
+scenario = CustomDataset(args, img_processor).get_scenario()
 
-text_logger = TextLogger(open('log_{args.data}_{args.strategy}.txt', 'a'))
+text_logger = TextLogger(open(f'log_{args.data}_{args.strategy}.txt', 'a'))
+interactive_logger = InteractiveLogger()
 
 eval_plugin = EvaluationPlugin(
-    accuracy_metrics(minibatch=True, epoch=True, experience=True, stream=True),
-    loss_metrics(minibatch=True, epoch=True, experience=True, stream=True),
-    timing_metrics(epoch=True),
-    cpu_usage_metrics(experience=True),
+    accuracy_metrics(minibatch=False, epoch=True, experience=True, stream=True),
+    loss_metrics(minibatch=False, epoch=True, experience=True, stream=True),
+    # timing_metrics(epoch=True),
+    # cpu_usage_metrics(experience=True),
     forgetting_metrics(experience=True, stream=True),
-    StreamConfusionMatrix(num_classes=args.num_classes, save_image=False),
-    disk_usage_metrics(minibatch=True, epoch=True, experience=True, stream=True),
-    loggers=[text_logger]
+    # StreamConfusionMatrix(num_classes=args.num_classes, save_image=False),
+    # disk_usage_metrics(minibatch=True, epoch=True, experience=True, stream=True),
+    loggers=[text_logger, interactive_logger]
 )
 
 optimizer = torch.optim.Adam(
@@ -81,11 +74,24 @@ optimizer = torch.optim.Adam(
         )
 criterion = nn.CrossEntropyLoss()
 
+if args.strategy == 'replay':
+    cl_strategy = Replay(
+        model, optimizer, criterion,
+        train_mb_size=args.batch_size, train_epochs=args.epochs, eval_mb_size=args.batch_size, evaluator=eval_plugin, device=args.device
+    )
+elif args.strategy == 'agem':
+    cl_strategy = AGEM(
+        model, optimizer, criterion,
+        train_mb_size=args.batch_size, train_epochs=args.epochs, eval_mb_size=args.batch_size, evaluator=eval_plugin, device=args.device
+    )
+elif args.strategy == 'ewc':
+    cl_strategy = EWC(
+        model, optimizer, criterion,
+        train_mb_size=args.batch_size, train_epochs=args.epochs, eval_mb_size=args.batch_size, evaluator=eval_plugin, device=args.device
+    )
+else:
+    ValueError()
 
-cl_strategy = Naive(
-    model, optimizer, criterion,
-    train_mb_size=100, train_epochs=1, eval_mb_size=100, evaluator=eval_plugin
-)
 
 # TRAINING LOOP
 print('Starting experiment...')
@@ -94,7 +100,7 @@ for experience in scenario.train_stream:
     print("Start of experience: ", experience.current_experience)
     print("Current Classes: ", experience.classes_in_this_experience)
 
-    res = cl_strategy.train(experience, num_workers = 2)
+    res = cl_strategy.train(experience, num_workers = args.num_workers)
     print('Training completed')
 
     print('Computing accuracy on the whole test set')
